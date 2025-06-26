0fb920a0f25b80a27cc77131e64bd186b3f46d2f
guicardoso <gccardosomiranda@outlook.com>
2025-06-26 17:25:09 -0400

    Player and Enemy
    Take Damage

--- godot-project/scripts/blob.gd
+++ godot-project/scripts/blob.gd
@@ -1,9 +1,25 @@
 extends CharacterBody2D
 
+@export var health = 3
 @export var speed = 300 
+@export var explosion_scene: PackedScene
 @onready var player = get_node("/root/Game/Player")
+
+func _ready() -> void:
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+
+func take_damage() -> void:
+	health -= 1
+	%Slime.play_hurt()
+	
+	if health == 0:
+		queue_free()
+		
+		var smoke = explosion_scene.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
--- godot-project/scripts/player.gd
+++ godot-project/scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+@export var health = 100.0
 @export var speed = 600
+const DAMAGE_RATE = 5.0
 
 func _process(delta: float) -> void:
 	if velocity.length() > 0:
@@ -12,3 +16,10 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
+	
+	var overlapping_blobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_blobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_blobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
